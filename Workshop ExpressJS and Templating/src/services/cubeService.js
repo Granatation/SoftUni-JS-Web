b8fa2470c8c5cb2b741059e242506ba8acde1ce6
Nikola Emilov <nikico248@gmail.com>
2022-05-28 13:33:58 +0300

    Updated ids

--- Workshop ExpressJS and Templating/src/services/cubeService.js
+++ Workshop ExpressJS and Templating/src/services/cubeService.js
@@ -5,7 +5,7 @@ const path = require('path');
 exports.getOne = (id) => cubes[id];
 
 exports.save = (cube) => {
-    cubes.push(cube);
+    cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
     let textData = JSON.stringify(cubes, '', 4);
 
     return fs.writeFile(path.resolve('Workshop ExpressJS and Templating', 'src/db.json'), textData, { encoding: 'utf-8' });
