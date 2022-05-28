810aa7b7a35947a30e6143f171caf6abf99632c6
Nikola Emilov <nikico248@gmail.com>
2022-05-28 14:09:02 +0300

    Search ready

--- Workshop ExpressJS and Templating/src/controllers/homeController.js
+++ Workshop ExpressJS and Templating/src/controllers/homeController.js
@@ -1,5 +1,9 @@
-const cubes = require('../db.json');
+const cubeService = require('../services/cubeService');
 
 exports.index = (req, res) => {
+    let { search, from, to } = req.query;
+
+    const cubes = cubeService.getAll(search, from, to)
+
     res.render('index', { cubes });
 };
--- Workshop ExpressJS and Templating/src/services/cubeService.js
+++ Workshop ExpressJS and Templating/src/services/cubeService.js
@@ -4,6 +4,15 @@ const path = require('path');
 
 exports.getOne = (id) => cubes[id];
 
+exports.getAll = (search, from, to) => {
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search ? search.toLowerCase() : ''))
+        .filter(x => from ? x.difficultyLevel >= from : true)
+        .filter(x => to ? x.difficultyLevel <= to : true)
+
+    return result;
+};
+
 exports.save = (cube) => {
     cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
     let textData = JSON.stringify(cubes, '', 4);
