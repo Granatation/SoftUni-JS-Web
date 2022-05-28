c3be3d56eaf44969efb16b5a7f1eb6553db744f2
Nikola Emilov <nikico248@gmail.com>
2022-05-28 11:18:35 +0300

    Added create cube page

--- Workshop ExpressJS and Templating/src/routes.js
+++ Workshop ExpressJS and Templating/src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
+const createController = require('./controllers/createController');
 
 const router = express.Router();
 
 router.get('/', homeController.index);
 router.get('/about', aboutController.index);
+router.get('/create', createController.index);
 
 module.exports = router;
