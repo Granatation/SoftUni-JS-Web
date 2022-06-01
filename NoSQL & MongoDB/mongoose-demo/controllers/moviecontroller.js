3795d9b4ad85df2fc7189e0c542ea0aac3f6f5d7
Nikola Emilov <nikico248@gmail.com>
2022-06-01 17:19:47 +0300

    Controllers and models added

---
+++ NoSQL & MongoDB/mongoose-demo/controllers/moviecontroller.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Movie } = require('../models/Movie')
+
+router.get('/', async(req, res) => {
+    const movies = await Movie.find().lean();
+
+    res.render('movies', { movies });
+});
+
+router.get('/:movieId', async(req, res) => {
+    console.log(req.params.movieId);
+
+    let movie = await Movie.findById(req.params.movieId).lean();
+
+    req.render('movieDetails', { movie });
+});
+
+router.get('/create', (req, res) => {
+    res.render('createMovie');
+});
+
+router.post('/create', (req, res) => {
+    let savedMovie = await Movie.create(req.body);
+    console.log(savedMovie);
+
+    res.redirect('/movies');
+})
+
+module.exports = router
---
+++ NoSQL & MongoDB/mongoose-demo/models/Movie.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    title: {
+        type: String,
+        required: [true, 'Movie title is required'],
+        minlength: 2
+    },
+    description: String,
+    genres: {
+        type: String,
+        enum: ['action', 'comedy', 'horror'],
+        required: false
+    },
+    imageUrl: String,
+    year: {
+        type: Number,
+        min: [1888, 'The year {VALUE} should not be higher than 1888'],
+        required: false
+    }
+});
+
+movieSchema.path('title').validate(function() {
+    return this.title.length >= 2 && this.title.length <= 20;
+}, "Movie title should be less than 20 characters and more than 2!");
+
+movieSchema.methods.getInfo = function() {
+    return `${this.title} - ${this.description ||'n/a'}`
+};
+
+movieSchema.virtual('isNew')
+    .get(function() {
+        return this.year >= 2020
+    });
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+exports.Movie = Movie;
