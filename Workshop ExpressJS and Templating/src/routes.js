436a8db97b0650435a9c7be323e8d370baa632d4
Nikola Emilov <nikico248@gmail.com>
2022-06-04 14:48:44 +0300

    Add attach accessory endpoint

--- Workshop ExpressJS and Templating/src/routes.js
+++ Workshop ExpressJS and Templating/src/routes.js
@@ -4,16 +4,19 @@ const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const createController = require('./controllers/createController');
 const detailsController = require('./controllers/detailsController');
-const accessoryController = require('./controllers/accessoryController')
+const accessoryController = require('./controllers/accessoryController');
+const attachController = require('./controllers/attachControler');
 
 const router = express.Router();
 
 router.get('/', homeController.index);
 router.get('/about', aboutController.index);
-router.get('/create', createController.indexGet);
-router.post('/create', createController.indexPost);
-router.get('/details/:id', detailsController.index);
+router.get('/cube/create', createController.indexGet);
+router.post('/cube/create', createController.indexPost);
+router.get('/cube/details/:id', detailsController.index);
 router.get('/accessory/create', accessoryController.index);
 router.post('/accessory/create', accessoryController.indexPost);
+router.get('/cube/:id/accessory', attachController.index);
+
 
 module.exports = router;
