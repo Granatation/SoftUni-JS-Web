417b026a68b362b5a290860ead7384357ee8e713
Nikola Emilov <nikico248@gmail.com>
2022-06-08 21:26:30 +0300

    Verify jwt

--- Session and Authentication/bcryptDemo.js
+++ Session and Authentication/bcryptDemo.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 //const password = 'mysecretpass';
 const hashedPassword = '$2b$10$Kw3zIv/1AZaugweDa8HPbufCp1bhvlQBXIeSFwmlP0oGX6jdPgSoS';
 const saltRounds = 10;
+const secret = 'MySecret';
 
 app.get('/hash/:password?', async(req, res) => {
     const salt = await bcrypt.genSalt(saltRounds);
@@ -23,7 +24,6 @@ app.get('/login/:password?', async(req, res) => {
     if (isValidPass) {
         const payload = { username: 'Pesho' };
         const options = { expiresIn: '2d' }
-        const secret = 'MySecret';
 
         const token = jwt.sign(payload, secret, options);
 
@@ -32,5 +32,17 @@ app.get('/login/:password?', async(req, res) => {
         res.send('Wrong password')
     }
 });
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlBlc2hvIiwiaWF0IjoxNjU0NzEyMzY0LCJleHAiOjE2NTQ4ODUxNjR9.9AFcAKDsRZ1kON4mSi805VZaf9f7_JgYgyCkx67LiGI
+app.get('/verify/:token', (req, res) => {
+    try {
+        let decodedToken = jwt.verify(req.params.token, secret);
+
+        res.json(decodedToken);
+    } catch (error) {
+        res.status(401).send(error);
+    }
+});
+
 
 app.listen(5000, () => console.log('Server listening on port 5000'))
