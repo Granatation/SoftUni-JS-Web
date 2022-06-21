f4db19997960773ffcf1c65764eb704511353477
Nikola Emilov <nikico248@gmail.com>
2022-06-21 16:09:35 +0300

    added token

--- Exam Preparation 1/controllers/authController.js
+++ Exam Preparation 1/controllers/authController.js
@@ -1,17 +1,20 @@
 const router = require('express').Router();
+
 const authService = require('../services/authService');
+const { COOKIE_SESSION_NAME } = require('../config/constants');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/register', async(req, res) => {
+router.post('/login', async(req, res) => {
     const { username, password } = req.body;
 
     const user = await authService.login(username, password)
     const token = await authService.createToken(user)
 
-
+    res.cookie(COOKIE_SESSION_NAME, token);
+    res.redirect('/')
 });
 
 router.get('/register', (req, res) => {
@@ -25,8 +28,11 @@ router.post('/register', async(req, res) => {
         return res.render('auth/register', { error: 'Password missmatch!' })
     }
     try {
-        await authService.create({ username, password, address })
-        res.redirect('/login')
+        const createdUser = await authService.create({ username, password, address })
+        const token = await authService.createToken(createdUser)
+
+        res.cookie(COOKIE_SESSION_NAME, token);
+        res.redirect('/login');
     } catch (error) {
         return res.render('auth/register', { error: 'db error!' })
     }
