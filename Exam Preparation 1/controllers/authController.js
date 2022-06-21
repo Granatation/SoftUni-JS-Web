723c4f1c6e0b83b9e58e26444cb3b8d14b16e323
Nikola Emilov <nikico248@gmail.com>
2022-06-21 15:57:42 +0300

    Auth functionality

--- Exam Preparation 1/controllers/authController.js
+++ Exam Preparation 1/controllers/authController.js
@@ -1,15 +1,36 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+router.post('/register', async(req, res) => {
+    const { username, password } = req.body;
+
+    const user = await authService.login(username, password)
+    const token = await authService.createToken(user)
+
+
 });
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
+router.post('/register', async(req, res) => {
+    const { username, password, repassword, address } = req.body;
+
+    if (password !== repassword) {
+        return res.render('auth/register', { error: 'Password missmatch!' })
+    }
+    try {
+        await authService.create({ username, password, address })
+        res.redirect('/login')
+    } catch (error) {
+        return res.render('auth/register', { error: 'db error!' })
+    }
+
 });
 
 module.exports = router;
---
+++ Exam Preparation 1/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { SALT_ROUNDS } = require('../config/env')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+});
+userSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, SALT_ROUNDS)
+        .then(hashedPass => {
+            this.password = hashedPass;
+
+            next();
+        });
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
