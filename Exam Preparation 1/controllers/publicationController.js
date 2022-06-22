9004b864778bdc4fd6993d44c48f51f47e629b92
Nikola Emilov <nikico248@gmail.com>
2022-06-22 21:19:10 +0300

    Publications service and controller

---
+++ Exam Preparation 1/controllers/publicationController.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const { isAuth } = require('../middlewares/authMiddleware');
+const publicationService = require('../services/publicationService');
+
+router.get('/', async(req, res) => {
+    const publications = await publicationService.getAll().lean();
+
+    res.render('publication', { publications });
+})
+
+router.get('/create', isAuth, (req, res) => {
+    res.render('publication/create');
+})
+
+router.post('/create', isAuth, async(req, res) => {
+    const publicationData = {...req.body, author: req.user }
+
+    await publicationService.create(publicationData);
+
+    res.redirect('/publications')
+})
+
+module.exports = router;
