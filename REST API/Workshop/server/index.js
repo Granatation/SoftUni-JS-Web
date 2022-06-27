fa206c7bf73387d9c3f051d3acfedae20b6985ce
Nikola Emilov <nikico248@gmail.com>
2022-06-27 21:24:10 +0300

    Some changes

--- REST API/Workshop/server/index.js
+++ REST API/Workshop/server/index.js
@@ -1,9 +1,27 @@
+const mongoose = require('mongoose');
 const express = require('express');
 
-const app = express();
+const cors = require('./src/middlewares/cors');
+const furnitureController = require('./src/controllers/furnitureController');
 
-app.get('/', (req, res) => {
-    res.json({ message: 'It works' });
-});
+async function start() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/furniture');
 
-app.listen(3030, () => console.log('REST service listening on port 3030'));
+        console.log('DB ready');
+    } catch (error) {
+        console.log('Error connecting to db');
+        return process.exit(1);
+    }
+
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.use('/data/catalog', furnitureController)
+
+    app.listen(3030, () => console.log('REST service listening on port 3030'));
+}
+
+start();
