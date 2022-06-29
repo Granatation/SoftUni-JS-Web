c563ec8f40046ef633ce7d07eb114345f6413265
Nikola Emilov <nikico248@gmail.com>
2022-06-29 21:08:46 +0300

    Babel used

---
+++ ReactJS/React and JSX/JSX-DEMO/build/script.js
@@ -0,0 +1,19 @@
+var rootElement = document.getElementById('root');
+var root = ReactDOM.createRoot(rootElement);
+
+// const reactElement = React.createElement(
+//     'header', { className: 'site-header' },
+//     React.createElement('h1', {}, 'Hello from React!')
+// );
+
+var reactElement = React.createElement(
+    'header',
+    { className: 'site-header' },
+    React.createElement(
+        'h1',
+        null,
+        'Hello from JSX!!!'
+    )
+);
+
+root.render(reactElement);
--- ReactJS/React and JSX/JSX-DEMO/src/script.js
+++ ReactJS/React and JSX/JSX-DEMO/src/script.js
@@ -1,9 +1,15 @@
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-const reactElement = React.createElement(
-    'header', { className: 'site-header' },
-    React.createElement('h1', {}, 'Hello from React!')
+// const reactElement = React.createElement(
+//     'header', { className: 'site-header' },
+//     React.createElement('h1', {}, 'Hello from React!')
+// );
+
+const reactElement = (
+    <header className='site-header'> 
+        <h1>Hello from JSX!!!</h1>
+    </header>
 );
 
-root.render(reactElement);
+     root.render(reactElement);
