52d6fd3a4622829a13b4245102788522f73fb7ad
Nikola Emilov <nikico248@gmail.com>
2022-07-16 12:22:37 +0300

    Structure

--- ReactJS/Workshop-Components/client/src/App.js
+++ ReactJS/Workshop-Components/client/src/App.js
@@ -1,10 +1,23 @@
-import { Header } from './components/Header'
+import { Header } from './components/common/Header'
+import { Footer } from './components/common/Footer'
+import { SearchBar } from './components/search/SearchBar';
+import { UserSection } from './components/user-section/UserSection';
 import './App.css'
 
 function App() {
     return (
         <div>
             <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <SearchBar />
+                    
+                    <UserSection/>
+                </section>
+            </main>
+
+            <Footer />
         </div>
     );
 }
