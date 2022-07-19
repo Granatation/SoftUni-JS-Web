f4df9624291c67da8f16333feff804de24b7f5f5
Nikola Emilov <nikico248@gmail.com>
2022-07-19 15:48:55 +0300

    summarized validators

--- ReactJS/Workshop-Components/client/src/components/user-list/user-create/UserCreate.js
+++ ReactJS/Workshop-Components/client/src/components/user-list/user-create/UserCreate.js
@@ -4,7 +4,7 @@ export const UserCreate = ({
     onClose,
     onUserCreate
 }) => {
-    const [firstNameHasError, setFirstNameHasError] = useState('')
+    const [errors, setErrors] = useState('')
     const [values, setValues] = useState({
         firstName: '',
         lastName: '',
@@ -34,13 +34,14 @@ export const UserCreate = ({
         onUserCreate(userData);
     }
 
-    const validateFirstName = (e) => {
-        if (values.firstName.length < 3) {
-            setFirstNameHasError(true);
+    const minLength = (e, bound) => {
+        if (values[e.target.name].length < bound) {
+            setErrors(state => ({
+                ...state,
+                [e.target.name]: true
+            }));
         }
-        else {
-            setFirstNameHasError(false);
-        }
+
     }
 
     return (
@@ -65,10 +66,10 @@ export const UserCreate = ({
                                 <label htmlFor="firstName">First name</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-user"></i></span>
-                                    <input id="firstName" name="firstName" type="text" value={values.firstName} onChange={changeHandler} onBlur={validateFirstName} />
+                                    <input id="firstName" name="firstName" type="text" value={values.firstName} onChange={changeHandler} onBlur={(e) => minLength(e, 3)} />
                                 </div>
                                 {
-                                    firstNameHasError &&
+                                    errors.firstName &&
                                     <p className="form-error">
                                         First name should be at least 3 characters long!
                                     </p>
@@ -78,11 +79,13 @@ export const UserCreate = ({
                                 <label htmlFor="lastName">Last name</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-user"></i></span>
-                                    <input id="lastName" name="lastName" type="text" value={values.lastName} onChange={changeHandler} />
+                                    <input id="lastName" name="lastName" type="text" value={values.lastName} onChange={changeHandler} onBlur={(e) => minLength(e, 3)} />
                                 </div>
-                                <p className="form-error">
-                                    Last name should be at least 3 characters long!
-                                </p>
+
+                                {errors.lastName &&
+                                    <p className="form-error">
+                                        Last name should be at least 3 characters long!
+                                    </p>}
                             </div>
                         </div>
 
