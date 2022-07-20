f98af12e06802bbf6ea6999f4f839eace4d1cb15
Nikola Emilov <nikico248@gmail.com>
2022-07-20 15:21:16 +0300

    Routes

---
+++ ReactJS/Routing/demo/src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Home } from './components/Home';
+import { About } from './components/About'
+import { Pricing } from './components/Pricing'
+import { Contacts } from './components/Contacts'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React Router</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="contacts" element={<Contacts />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
