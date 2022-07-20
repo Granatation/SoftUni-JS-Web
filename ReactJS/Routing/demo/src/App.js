4a1e4cba3f4c87d90108030b89f5b6697e29c6aa
Nikola Emilov <nikico248@gmail.com>
2022-07-20 15:36:44 +0300

    Navigation Link

--- ReactJS/Routing/demo/src/App.js
+++ ReactJS/Routing/demo/src/App.js
@@ -4,17 +4,24 @@ import { Home } from './components/Home';
 import { About } from './components/About'
 import { Pricing } from './components/Pricing'
 import { Contacts } from './components/Contacts'
+import { NotFound } from './components/NotFound';
+import { Navigation } from './components/Navigation';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React Router</h1>
 
+      <Navigation />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/pricing/*" element={<Pricing />} />
+        <Route path="/pricing/contacts" element={<Contacts />} />
         <Route path="contacts" element={<Contacts />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
