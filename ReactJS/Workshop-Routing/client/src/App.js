7e615308fbfa9e5b56f318e4aa5afa75a70d0840
Nikola Emilov <nikico248@gmail.com>
2022-07-22 12:44:37 +0300

    Catalog page rendered dynamically
    class > className

--- ReactJS/Workshop-Routing/client/src/App.js
+++ ReactJS/Workshop-Routing/client/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 import { Header } from './components/Header/Header'
 import { Home } from './components/Home/Home'
@@ -6,10 +7,21 @@ import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { Create } from './components/Create/Create';
 import { Catalog } from './components/Catalog/Catalog';
+import * as gameService from './services/gameService';
 
 import './App.css';
 
+
 function App() {
+  const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result);
+            })
+    }, []);
+
   return (
     <div id="box">
 
@@ -17,11 +29,11 @@ function App() {
 
       <main id="main-content">
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={<Home games={games}/>}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/register' element={<Register />}/>
           <Route path='/create' element={<Create />}/>
-          <Route path='/catalog' element={<Catalog />}/>
+          <Route path='/catalog' element={<Catalog games={games}/>}/>
         </Routes>
       </main>
 
--- ReactJS/Workshop-Routing/client/src/components/Create/Create.js
+++ ReactJS/Workshop-Routing/client/src/components/Create/Create.js
@@ -1,8 +1,8 @@
 export const Create = () => {
     return (
-        <section id="create-page" class="auth">
+        <section id="create-page" className="auth">
             <form id="create">
-                <div class="container">
+                <div className="container">
 
                     <h1>Create Game</h1>
                     <label for="leg-title">Legendary title:</label>
@@ -19,7 +19,7 @@ export const Create = () => {
 
                     <label for="summary">Summary:</label>
                     <textarea name="summary" id="summary"></textarea>
-                    <input class="btn submit" type="submit" value="Create Game" />
+                    <input className="btn submit" type="submit" value="Create Game" />
                 </div>
             </form>
         </section>
